833322c9a2b66ae0d7ff991a5cd3cc10ac94f414
Melo <toosho@me.com>
2013-11-24 23:34:29 +0100

    Add Configure

--- proxy/recipes/configure.rb
+++ proxy/recipes/configure.rb
@@ -21,14 +21,14 @@
 package "proxy"
 
 template "#{node[:nginx][:dir]}/conf.d/proxy.conf" do
-  source "proxy.conf.erb"
-  owner "root"
-  group "root"
-  mode 0644
+	source "proxy.conf.erb"
+	owner "root"
+	group "root"
+	mode 0644
 end
 
 include_recipe "nginx::service"
 
 service "nginx" do
-  action [ :enable, :start ]
+	action [ :enable, :start ]
 end
