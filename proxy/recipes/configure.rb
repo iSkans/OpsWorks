bebeb1cc0424ccd2e110a1be89509bded0cc9bfa
Melo <toosho@me.com>
2013-11-25 00:01:55 +0100

    Add Proxy Configuration to Nginx

--- proxy/recipes/configure.rb
+++ proxy/recipes/configure.rb
@@ -3,10 +3,13 @@ template "#{node[:nginx][:dir]}/conf.d/proxy.conf" do
 	owner "root"
 	group "root"
 	mode 0644
+	not_if do 
+      ::File.exists?("#{node[:nginx][:dir]}/conf.d/proxy.conf")
+    end
 end
 
 include_recipe "nginx::service"
 
 service "nginx" do
-	action [ :enable, :start ]
+	action [ :reload ]
 end
