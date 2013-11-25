b8ad110d7a9247107be3703c0057e3610206c548
Melo <toosho@me.com>
2013-11-25 18:43:34 +0100

    Add Deploy and Undeploy Proxy

--- proxy/recipes/deploy.rb
+++ proxy/recipes/deploy.rb
@@ -1,12 +1,11 @@
 include_recipe 'deploy'
 include_recipe "nginx::service"
 
-Chef::Log.info("Deploy Proxy Configuration to Nginx.")
 node[:deploy].each do |application, deploy|
 	if deploy[:application_type] != 'nodejs'
 		next
 	end	
-	Chef::Log.info("Deploy Proxy Configuration to Nginx.")
+	Chef::Log.info("Deploy Nginx Proxy Configuration for #{application}.")
 	execute "proxy2ensite #{application} #{deploy[:deploy_to]}" do
 		command "/usr/sbin/proxy2ensite #{application} #{deploy[:deploy_to]} #{deploy[:domains].join(',')}"
 		notifies :reload, "service[nginx]"
--- proxy/recipes/undeploy.rb
+++ proxy/recipes/undeploy.rb
@@ -1,31 +1,17 @@
-#
-# Cookbook Name:: deploy
-# Recipe:: web-undeploy
-
 include_recipe 'deploy'
-include_recipe 'nginx::service'
+include_recipe "nginx::service"
 
 node[:deploy].each do |application, deploy|
-  if deploy[:application_type] != 'nodejs'
-    Chef::Log.debug("Skipping undeploy::nodejs-proxy for application #{application} as it is not a node.js app")
-    next
-  end
-  
-  link "#{node[:nginx][:dir]}/sites-enabled/#{application}" do
-    action :delete
-    only_if do 
-      ::File.exists?("#{node[:nginx][:dir]}/sites-enabled/#{application}")
-    end
-    notifies :restart, "service[nginx]"
-  end
-  
-  directory "#{deploy[:deploy_to]}" do
-    recursive true
-    action :delete
-    only_if do 
-      ::File.exists?("#{deploy[:deploy_to]}")
-    end
-  end
+	if deploy[:application_type] != 'nodejs'
+		next
+	end	
+	Chef::Log.info("Undeploy Nginx Proxy Configuration for #{application}.")
+	execute "proxy2dissite #{application}" do
+		command "/usr/sbin/proxy2dissite #{application}"
+		notifies :reload, "service[nginx]"
+	end
 end
+
+
 
 
