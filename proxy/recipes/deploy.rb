a2c3a7c5f67868303d1ce4033b8014c8165d62b2
Melo <toosho@me.com>
2013-11-25 16:14:45 +0100

    Add Domains

--- proxy/recipes/deploy.rb
+++ proxy/recipes/deploy.rb
@@ -6,12 +6,7 @@ node[:deploy].each do |application, deploy|
 	if deploy[:application_type] != 'nodejs'
 		next
 	end	
-	Chef::Log.info("Deploy To : #{deploy[:deploy_to]}")
-	Chef::Log.info("Application : #{application}")
-	Chef::Log.info("Application name : #{deploy[:application_name]}")
-	Chef::Log.info("Application name2 : #{deploy[:name]}")
-	Chef::Log.info("Application domains : #{deploy[:domains]}")
-	Chef::Log.info("Application domains : #{deploy}")
+	Chef::Log.info("Application domains : #{deploy[:domains].join(',')}")
 	Chef::Log.info("Deploy Proxy Configuration to Nginx.")
 	execute "proxy2ensite #{application} #{deploy[:deploy_to]}" do
 		command "/usr/sbin/proxy2ensite #{application} #{deploy[:deploy_to]} #{deploy[:application_name]}"
