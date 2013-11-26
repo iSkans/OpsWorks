f0ee35af4bcad489c35dfb444ec270310723ef3e
Melo <toosho@me.com>
2013-11-26 09:03:02 +0100

    Add automatically restart nodeJs App

--- proxy/recipes/deploy.rb
+++ proxy/recipes/deploy.rb
@@ -10,5 +10,17 @@ node[:deploy].each do |application, deploy|
 		command "/usr/sbin/proxy2ensite #{application} #{deploy[:deploy_to]} #{deploy[:domains].join(',')}"
 		notifies :reload, "service[nginx]"
 	end
+	
+	Chef::Log.info("Restart NodeJS Application #{application}.")
+	execute "Restart NodeJS Application #{application}." do
+		cwd deploy[:current_path]
+		command "sleep #{node[:deploy][application][:nodejs][:sleep_before_restart]} && #{node[:deploy][application][:nodejs][:restart_command]}"
+		action :run
+		only_if do 
+			File.exists?(deploy[:current_path])
+		end
+	end
+  end
+	
 end
 
