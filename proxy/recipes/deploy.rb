580d735f233c22fdf50095bd7eaf7634bd5c9bfb
Melo <toosho@me.com>
2013-11-26 10:25:22 +0100

    Deploy Default Nginx Website

--- proxy/recipes/deploy.rb
+++ proxy/recipes/deploy.rb
@@ -1,10 +1,16 @@
 include_recipe 'deploy'
 include_recipe "nginx::service"
 
+Chef::Log.info("Deploy Default Website")
+execute "nxensite default" do
+	command "/usr/sbin/nxensite default"
+	notifies :reload, "service[nginx]"
+end
+
 node[:deploy].each do |application, deploy|
 	if deploy[:application_type] != 'nodejs'
 		next
-	end	
+	end
 	Chef::Log.info("Deploy Nginx Proxy Configuration for #{application}.")
 	execute "proxy2ensite #{application} #{deploy[:deploy_to]}" do
 		command "/usr/sbin/proxy2ensite #{application} #{deploy[:deploy_to]} #{deploy[:domains].join(',')}"
